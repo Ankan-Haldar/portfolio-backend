34c4eb6ed71e310c39fdda92721e8c0ac652a992
Ankan Haldar <your-email@example.com>
2026-05-03 22:50:42 +0530

    fix: add Pillow dependency

--- backend/settings.py
+++ backend/settings.py
@@ -99,3 +99,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://portfolio-backend-ww34.onrender.com"
+]
