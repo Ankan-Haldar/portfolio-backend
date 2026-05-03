81f3c59958a18c56d008fc62b93d49868e6e4676
Ankan Haldar <your-email@example.com>
2026-05-03 23:48:03 +0530

    change image field to URLField

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='projects/')
+    image = models.URLField()
     description = models.TextField(blank=True)
     github = models.URLField(blank=True)
     live = models.URLField(blank=True)
@@ -11,7 +11,7 @@ class Project(models.Model):
         return self.title
 class Skill(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='skills/')
+    image = models.URLField()
 
     def __str__(self):
         return self.title
